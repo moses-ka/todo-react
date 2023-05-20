73fcd54789d1efeb06ab3d56e5b58976cbccb4fe
moses-ka <ji.moses.ka@gmail.com>
2023-05-20 16:09:26 +0200

    local storge working with out posting

--- src/comp/addTask.js
+++ src/comp/addTask.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import addIcon from "../assets/add.png";
 import Tasks from "./tasks";
-import ".//style/addTask.css";
-
+import "./style/addTask.css";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
   const [savedTasks, setSavedTasks] = useState([]);
+  useEffect(() => {
+    if (savedTasks.length !== 0 ) {
+      
+    
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    console.log(savedTasks);
+    }
+  }, [savedTasks]);
+
+  // useEffect(() => {
+
+  //   console.log(localStorage.getItem("tasks"));
+  // }, [savedTasks]);
+
+ 
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -20,39 +34,48 @@ const AddTask = () => {
     setSavedTasks(newTasks);
   };
 
+  const handleEdit = (index, text) => {
+    const newTasks = [...savedTasks];
+    newTasks[index] = text;
+    setSavedTasks(newTasks);
+  };
+
   return (
     <>
-      <div className="add-task-container">
-        <h1 className="text-Title">Add Task</h1>
+      <div className="wrapper">
+        <div className="add-task-container">
+          <h2 className="text-Title">Add Task</h2>
 
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder="Add Task"
-            className="input"
-            type="text"
-            value={task}
-            onChange={(event) => setTask(event.target.value)}
-          />
-          <button type="submit">
-            <img
-              className="img-add"
-              width={"30px"}
-              height={"30px"}
-              src={addIcon}
-              alt=""
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Add Task"
+              className="input"
+              type="text"
+              value={task}
+              onChange={(event) => setTask(event.target.value)}
             />
-          </button>
-        </form>
-      </div>
-      <div className="tasks-container">
-        {savedTasks.map((task, index) => (
-          <Tasks
-            key={index}
-            text={task}
-            index={index}
-            onDelete={handleDelete}
-          />
-        ))}
+            <button type="submit">
+              <img
+                className="img-add"
+                width={"30px"}
+                height={"30px"}
+                src={addIcon}
+                alt=""
+              />
+            </button>
+          </form>
+        </div>
+        <div className="tasks-container">
+          {savedTasks.map((task, index) => (
+            <Tasks
+              key={index}
+              text={task}
+              index={index}
+              onDelete={handleDelete}
+              onEdit={handleEdit}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/comp/tasks.js
+++ src/comp/tasks.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ImgDel from "../assets/x-mark.png";
 
-
 const Tasks = (props) => {
+  const [isEditable, setIsEditable] = useState(false);
+  const [text, setText] = useState(props.text);
+
   const handleDeleteClick = () => {
     props.onDelete(props.index);
   };
 
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleInputBlur = () => {
+    setIsEditable(false);
+    props.onEdit(props.index, text);
+  };
+
   return (
     <>
-      <p className="Ptasks" contentEditable="true">{props.text}</p>
-      <img className="img-del"
+      {isEditable ? (
+        <input
+          className="input-tasks"
+          type="text"
+          value={text}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          autoFocus
+        />
+      ) : (
+        <p className="Ptasks" onClick={() => setIsEditable(true)}>
+          {text}
+        </p>
+      )}
+      <img
+        className="img-del"
         onClick={handleDeleteClick}
-       
         src={ImgDel}
         alt=""
       />
